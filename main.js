c227c78049a4c90d045266f6711321e22154255d
Ismael Patrick <101072263+Ismaelp96@users.noreply.github.com>
2022-04-03 13:32:16 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,104 @@
+/* open and close menu */
+
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/* when click in item for menu, hidden the menu */
+
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+/* change the header page when scrolling */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+function changeHeaderWhenScroll() {
+  if (window.scrollY >= navHeight) {
+    // scroll is greater than header height
+    header.classList.add('scroll')
+  } else {
+    // smaller than header height
+    header.classList.remove('scroll')
+  }
+}
+
+/*  SWIPER */
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
+})
+
+/*  scrollReveal: show elements when in scrolling the page */
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 600,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .text, #home .image, #about-us .image, #about-us .text, #services header, #services .card, #testimony header, #testimony .testimonials, #contact .text, #contact .links footer .brand, footer`,
+  { interval: 100 }
+)
+
+/* button back to the top */
+const backToTopButton = document.querySelector('.back-to-top')
+
+function backToTop() {
+  if (this.window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+/* MENU ACTIVE VISIBLE ON PAGE */
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
+/* When Scroll */
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+})
